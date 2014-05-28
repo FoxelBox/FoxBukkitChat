00b74ba57b1a788d93215ebed2af2baba870af81
Doridian Draconia <mriq91@gmail.com>
2014-05-28 23:41:28 +0200

    Revert to CB guava

--- src/main/java/de/doridian/yiffbukkit/chatcomponent/FishBansResolver.java
+++ src/main/java/de/doridian/yiffbukkit/chatcomponent/FishBansResolver.java
@@ -16,9 +16,9 @@
  */
 package de.doridian.yiffbukkit.chatcomponent;
 
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import com.mojang.api.profiles.HttpProfileRepository;
 import com.mojang.api.profiles.Profile;
 
@@ -28,7 +28,7 @@ import java.util.concurrent.TimeUnit;
 public class FishBansResolver {
 	private static final HttpProfileRepository HTTP_PROFILE_REPOSITORY = new HttpProfileRepository("minecraft");
 
-	private static final LoadingCache<String, UUID> playerUUIDMap = CacheBuilder.newBuilder().expireAfterAccess(10, TimeUnit.MINUTES).softValues().build(new CacheLoader<String, UUID>() {
+	private static final Cache<String, UUID> playerUUIDMap = CacheBuilder.newBuilder().expireAfterAccess(10, TimeUnit.MINUTES).softValues().build(new CacheLoader<String, UUID>() {
 		@Override
 		public UUID load(String username) throws Exception {
 			Profile[] profiles = HTTP_PROFILE_REPOSITORY.findProfilesByNames(username);
